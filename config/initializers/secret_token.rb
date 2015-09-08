365aa58bd89e5cbdab6b2e075892eb18bafe70fe
Grant George <grantelliottgeorge@gmail.com>
2015-09-07 18:19:18 -0700

    init

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,10 +10,10 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 
-# Although this is not needed for an api-only application, rails4 
-# requires secret_key_base or secret_token to be defined, otherwise an 
+# Although this is not needed for an api-only application, rails4
+# requires secret_key_base or secret_token to be defined, otherwise an
 # error is raised.
 # Using secret_token for rails3 compatibility. Change to secret_key_base
 # to avoid deprecation warning.
 # Can be safely removed in a rails3 api-only application.
-RailsAngular::Application.config.secret_token = '3a66773a67ac2086fd1062c1c20ee4b05fe6debb1818f1ef14e042952d509915000504d3dbd68ae52043739f506922808805f9815a3df419f591dfde30c5f4d0'
+KnowEx::Application.config.secret_token = '3a66773a67ac2086fd1062c1c20ee4b05fe6debb1818f1ef14e042952d509915000504d3dbd68ae52043739f506922808805f9815a3df419f591dfde30c5f4d0'
