aa63de4e507eace979d7238d897259deadb2fa21
Rob Reinold <rreinold@gmail.com>
2015-10-01 02:50:23 -0500

    profiles.controller.js snapshot

--- client/src/app/components/profiles/profiles.controller.js
+++ client/src/app/components/profiles/profiles.controller.js
@@ -2,16 +2,16 @@
   'use strict';
 
   angular.module('know-ex')
-    .controller('ProfilesController', function ($scope, Profiles) {
+  .controller('ProfilesController', function ($scope, Profiles) {
 
-console.log('profile controller');
+		console.log('profile controller');
 
-      Profiles.get({profileId: $stateParams.profileId},function (res) {
-        $scope.profile = res;
-        console.log($scope.profile);
+      	Profiles.get(function (res) {
+				        $scope.profile = res;
+				        console.log($scope.profile);
 
-        $scope.intro = $scope.profile.intro;
-        console.log($scope.intro);
+				        $scope.intro = $scope.profile.intro;
+				        console.log($scope.intro);
 
     });
-})();
+})}();
