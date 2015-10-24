9ac8b2ae1226eb2b62018a1420c9ac98735fe1a5
Robert Reinold <rreinold@gmail.com>
2015-10-24 14:07:06 -0500

    Reverting changes to endorsement serializer

--- app/serializers/endorsement_serializer.rb
+++ app/serializers/endorsement_serializer.rb
@@ -1,3 +1,5 @@
 class EndorsementSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :text
+  has_one :user, serializer: UserShortSerializer
+  has_many :tags, serializer: TagSerializer
 end
